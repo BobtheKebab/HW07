a910b56e175a2a177750f9bdebdaef81f5104347
BobtheKebab <ahbab.ashraf@gmail.com>
2017-10-28 17:23:39 -0400

    Finished getRand

--- hw07.c
+++ hw07.c
@@ -7,18 +7,16 @@
 
 int getRand() {
   int fd = open("/dev/random", O_RDONLY);
-  //might be a problem with ret
-  int * ret;
-  read(fd, ret, 4); //bc an int is 4 bytes
-  printf("a is: %d\n", *ret);
+  int ret;
+  read(fd, &ret, 4); //bc an int is 4 bytes
   close(fd);
-  return *ret;
+  return ret;
 }
 
 int main() {
 
-  int * ;
-  printf("%d\n", sizeof(getRand()));
+  int num = getRand();
+  printf("%d\n", getRand());
 
 }
 
