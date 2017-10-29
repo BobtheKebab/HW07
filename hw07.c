91bc88429cd3c2147cb19ac4cc0b9778ef5296f3
Kristin Lin <klin11@stuy.edu>
2017-10-29 11:02:21 -0400

    finished assignment

--- hw07.c
+++ hw07.c
@@ -5,35 +5,45 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+// open /dev/random as read only; read 4 byes (int) into int ret. 
 int getRand() {
   int fd = open("/dev/random", O_RDONLY);
   int ret;
-  read(fd, &ret, 4); //bc an int is 4 bytes
+  read(fd, &ret, 4); 
   close(fd);
   return ret;
 }
 
+
 int main() {
+  
+  int arrR[10], arrW[10];
+  int count;
 
-  int ary[10], testAry[10];
-  int count = 0;
-
-  printf("Filling and printing array:\n");
-  while (count < 10) {
-    ary[count] = getRand();
-    printf("%d : %d\n", count, ary[count]);
-    count++;
+  // read from /dev/random 10 times; printing as the file is being read
+  printf("\nGenerating random numbers:\n");
+  for (count = 0; count < 10; count++) {
+    arrR[count] = getRand();
+    printf("arrR[%d] : %d\n", count, arrR[count]);
   }
 
-  printf("\nWriting the numbers to a file\n");
-  int fd = open("numbers", O_WRONLY | O_CREAT);
-  write(fd, ary, 40);
+  //create file with rwxr--r--; writes from arrR to file 40 bytes (10 ints); 
+  printf("\nWriting numbers to a file...\n");
+  int fd = open("numbers", O_WRONLY | O_CREAT, 0744);
+  write(fd, arrR, 40);
   close(fd);
 
-  printf("\nReading file and storing values in new array\n");
+  //open file created for read only; read from file 40 bytes into arrW
+  printf("\nReading numbers from file...\n");
+  fd = open("numbers", O_RDONLY);
+  read(fd, arrW, 40);
+  close(fd);
 
-  printf("\nPrinting new array to verify it worked:\n");
-  
+  //for loop to print each value in arrW; should match arrR
+  printf("\nVerification that written values were the same:\n");
+  for (count = 0; count < 10; count++) {
+    printf("arrW[%d] : %d\n", count, arrW[count]);
+  }
 }
 
 
